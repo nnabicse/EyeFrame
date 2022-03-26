af198d7f135ba5d1bd88acf5cc87703160ad9232
Nurun Nabi <nnabi.cse@gmail.com>
2022-03-26 20:06:48 +0600

    same item multiple selection erro solved

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -13,9 +13,17 @@ const Shop = () => {
 
     }, [])
 
-    const handleAddToCart  = (product) =>{
-        const newCart = [...cart, product]
-        setCart(newCart);
+    const handleAddToCart  = (selectedProduct) =>{
+        const exist = cart.find(product=>product.id === selectedProduct.id);
+        if(exist){
+            alert("Product Already Exist in Cart !!")
+
+        }else{
+            const newCart = [...cart, selectedProduct]
+            setCart(newCart);
+            
+        }
+        
     }
 
     const handleChooseOne = (cart) =>{
@@ -44,7 +52,7 @@ const Shop = () => {
             <div className='col col-sm-12 col-md-3 col-lg-3  a'>
                 <div className='cart-container'>
                 <h4 className='fw-bolder text-center p-2 mt-1'>Order Summary</h4>
-                <div className='my-5'>
+                <div className='my-2'>
                 {
                     cart.map(product=><Cart product={product}></Cart>)
                 }
