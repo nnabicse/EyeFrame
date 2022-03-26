a6cd1a41237129d630740c75e97983a3f1045531
Nurun Nabi <nnabi.cse@gmail.com>
2022-03-26 22:02:51 +0600

    question one complete

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Question.css'
+import reactImage from '../../images/react.jpg'
 
 const Question = () => {
     return (
@@ -10,7 +11,9 @@ const Question = () => {
             <div className='question'>
             <h1>How React JS Works: </h1>
             <p className='text-justify'>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora nobis beatae doloribus, explicabo odit enim illo doloremque perspiciatis hic sit, dolorem suscipit exercitationem recusandae, culpa sequi asperiores architecto voluptates harum temporibus sed! Cum labore fugit ipsa ab, ea laborum, veniam architecto libero nihil, ratione quis beatae possimus optio dolores repellendus perferendis eos hic qui. Ratione molestias quod itaque laborum recusandae quis labore omnis modi architecto nulla dolore veritatis, voluptate fugiat voluptatem, illum quo, dicta nam assumenda! Praesentium similique repudiandae et.
+            React is a JS library that used to create user interfaces like single page application in an efficient way using declarative code. It lets us build complex UIs from small and isolated pieces of code called components. React load data for the site once and then if any update happens in the site, only the targated component will refresh, not the whole website. <br /><br />
+            <img className='img-fluid p-3' src={reactImage} alt="" /><br /><br />
+            Each time we we update any data in our website react uses an  algorithm called Diff Algorithm to compare the existing DOM with a Virtual DOM which react manages under the hood. After comparison react chages only the components that does not match with the virtual DOM. Each React component is encapsulated and can operate independently. In React there are state that can be changed by the user and the are side effects of the change of state which used to make changes on the UI as the user wants. React always stalks a virtual DOM under the hood and if any data change happens in the DOM, react instantly compares the current DOM with the virtual DOM and updates on the real DOM.
             </p>
             </div>
             </div>
